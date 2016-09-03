c883a4cc7e6ba5a82726b2840193237255f62c77
Mikael Karon <mikael@karon.se>
2016-09-03 10:27:35 +0200

    feat: added gulp support

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require("gulp");
+var concat = require("gulp-concat");
+
+gulp.task("default", function() {
+  return gulp.src([
+    "bower_components/mu-jquery-wire/jquery.wire.js",
+    "bower_components/mu-jquery-crank/v2.0.2/jquery.crank.js",
+    "bower_components/mu-compose/compose.js",
+    "bower_components/mu-compose/constructor.js",
+    "bower_components/mu-compose/prototype.js",
+    "bower_components/mu-compose/regexp.js",
+    "bower_components/mu-jquery-hub/hub.js",
+    "bower_components/mu-jquery-widget/widget.js",
+    "bower_components/mu-jquery-widget/compose.js",
+    "bower_components/mu-jquery-widget/jquery.weave.js",
+    "./compose.js",
+    "./hub.js",
+    "./jquery.weave.js"
+  ])
+    .pipe(concat("all.js"))
+    .pipe(gulp.dest("./dist/"));
+});

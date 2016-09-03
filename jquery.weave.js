9440235bccbd089acd02dd239a625297d784e892
Mikael Karon <mikael@karon.se>
2016-09-03 03:33:27 +0200

    feat: use mu-jquery-app/jquery.weave

---
+++ jquery.weave.js
@@ -0,0 +1,28 @@
+(function(modules, root, factory) {
+  root["mu-jquery-app/jquery.weave"] = factory.apply(root, modules.map(function(m) {
+    return {
+        "jquery": jQuery
+      }[m] || root[m];
+  }));
+})([
+  "jquery",
+  "mu-jquery-widget/jquery.weave",
+  "mu-jquery-crank/jquery.crank"
+], this, function($, weave, crank) {
+  var slice = Array.prototype.slice;
+
+  return function(attr, callback) {
+    // weave elements from `attr` attribute
+    return weave.apply(this, arguments)
+      // convert widgets to $widgets
+      .then(function() {
+        return $($.map(arguments, function(widget) {
+          return ($.isArray(widget) ? widget[0] : widget).$element;
+        }));
+      })
+      // crank `initialize` on `$widgets`
+      .then(function($widgets) {
+        return crank.call($widgets, attr, "initialize");
+      });
+  }
+});

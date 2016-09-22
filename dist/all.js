324f6d73b7b946f4839dd9e1012a21574e35010c
Mikael Karon <mikael@karon.se>
2016-09-22 02:45:17 +0200

    feat(deps): bumped dependencies
    Bumped dependencies for `mu-jquery-component` to support configurable `create` in `jquery.wire`

--- dist/all.js
+++ dist/all.js
@@ -116,6 +116,13 @@
   return function(attr, callback) {
     var args = slice.call(arguments, 2);
     var count = 0;
+    var _create = create;
+    var _callback = callback;
+
+    if(!$.isFunction(callback)) {
+      _create = callback.create || _create;
+      _callback = callback.callback;
+    }
 
     return wire.call(this,
       function($element) {
@@ -124,8 +131,8 @@
       function($element, index, module) {
         var self = this;
 
-        return $.when(callback.call(self, module, index)).then(function(result) {
-          result = create(result, [$element, module = module + "@" + ++count].concat(args));
+        return $.when(_callback.call(self, module, index)).then(function(result) {
+          result = _create.call(self, result, [$element, module = module + "@" + ++count].concat(args));
 
           $element.attr(attr, function(i, value) {
             value = value.split(re_space);

c49a1b5e765b3cc8485a65d87cb7df547ad762f4
Mikael Karon <mikael@karon.se>
2016-10-15 17:53:43 +0200

    feat(deps): bumped deps
    - mu-jquery-widget@7.1.0

--- dist/all.js
+++ dist/all.js
@@ -328,26 +328,15 @@
         .join(" ");
     }
 
-    return [function ($element, ns) {
-      var me = this;
+    var widget = {
+      "off": function (events, selector, handler) {
+        var me = this;
+        me.$element.off(name.call(events, me.ns), selector, handler);
+      }
+    };
 
-      me.ns = ns;
-      me.$element = $element;
-
-      $.each(me.constructor.dom, function (index, op) {
-        switch (op.method) {
-          case "on":
-            me.on(op.events, op.selector, op.data, op.handler);
-            break;
-
-          case "attr":
-          case "prop":
-            $element[op.method](op.name, op.value);
-            break;
-        }
-      });
-    }, {
-      "on": function (events, selector, data, handler) {
+    ["on", "one"].forEach(function (op) {
+      this[op] = function (events, selector, data, handler) {
         var me = this;
 
         switch (arguments.length) {
@@ -366,16 +355,35 @@
             throw new Error("not enough arguments");
         }
 
-        me.$element.on(name.call(events, me.ns), selector, data, $.proxy(handler, me));
-      },
-      "off": function (events, selector, handler) {
-        var me = this;
-        me.$element.off(name.call(events, me.ns), selector, handler);
+        me.$element[op](name.call(events, me.ns), selector, data, $.proxy(handler, me));
       }
-    }];
+    }, widget);
+
+    return [function ($element, ns) {
+      var me = this;
+
+      me.ns = ns;
+      me.$element = $element;
+
+      $.each(me.constructor.dom, function (index, op) {
+        switch (op.method) {
+          case "on":
+          case "one":
+            me[op.method](op.events, op.selector, op.data, op.handler);
+            break;
+
+          case "attr":
+          case "prop":
+            $element[op.method](op.name, op.value);
+            break;
+        }
+      });
+    }, widget];
   });
 
   umd("mu-jquery-widget/dom")(["mu-create/regexp"], this, function (regexp) {
+    var re_on = /^one?$/;
+
     function copy(o) {
       return Object.keys(o).reduce(function (result, key) {
         if (!result.hasOwnProperty(key)) {
@@ -385,14 +393,14 @@
       }, this);
     }
 
-    return regexp(/^(on|attr|prop)\/(.+?)(?:\((.*)\))?$/, function (result, data, method, type, args) {
+    return regexp(/^(one?|attr|prop)\/(.+?)(?:\((.*)\))?$/, function (result, data, method, type, args) {
       var dom = toString.call(data.value) === "[object Object]"
         ? data.value
-        : method === "on"
+        : re_on.test(method)
           ? { "handler": data.value }
           : { "value": data.value };
 
-      dom = copy.call(dom, method === "on"
+      dom = copy.call(dom, re_on.test(method)
         ? {
           "method": method,
           "events": type,
